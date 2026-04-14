c577cf89ef8ce637502abaed674916e3773176c4
Ike Renson Salcedo Landong <iklandong@my.cspc.edu.ph>
2026-04-15 07:42:45 +0800

    full code cahnges in the andngike branch

--- lib/homepage/landingpage.dart
+++ lib/homepage/landingpage.dart
@@ -14,22 +14,38 @@ class _LandingpageState extends State<Landingpage> {
      body:Column(
       children: [
         Container(
-          padding: const EdgeInsets.all(10),
+          padding: const EdgeInsets.all(5 ),
           child:Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children:[
               Container(
+                margin: EdgeInsets.only(left: 10),
                 child: Image.network('assets/BRGHGMC.png', height: 100,width: 100,),
               ), 
               Container(
+              
+                margin: EdgeInsets.all(12),
                 child: Row(
-                   mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: const [
-                    Text('Text 1'),
-                    Text('Text 2'), 
-                    Text('Text 3'), 
-                    Text('Text 4'),  
-                    Text('Text 5'),  
+                   
+                  children: [
+                    SizedBox(height: 30 ),
+                    Container(
+                      margin: EdgeInsets.all(30),
+                      child: Text('GreenCross', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)), 
+                    ),   
+                     Container(
+                      margin: EdgeInsets.all(30),
+                      child: Text('Hospital Essentials', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)), 
+                     ),
+                      Container(
+                        margin: EdgeInsets.all(30),
+                        child: Text('Midterm Exam', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)), 
+                      ),
+                       Container(
+                        margin: EdgeInsets.all(30),
+                        child: Text('Entrance Exam', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)), 
+                       ),
+                       
                   ],
                 ),
               ),
@@ -37,8 +53,37 @@ class _LandingpageState extends State<Landingpage> {
               Container(
                 child: Row(
                   children: [
-                    IconButton(onPressed: (){}, icon: const Icon(Icons.search)),
-                    IconButton(onPressed: (){}, icon: const Icon(Icons.account_circle)),
+                    Container(
+                     // margin: EdgeInsets.all(12),
+                      child: TextButton(
+  onPressed: () {},
+  style: TextButton.styleFrom(
+    padding: EdgeInsets.all(16.0),
+    
+    side: const BorderSide(color: Colors.black, width: 1.0),
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.zero,
+    ),
+  ),
+  child: const Text('Login', style: TextStyle(color: Colors.black)),
+)
+                    ), 
+                    
+                    Container(
+                      margin: EdgeInsets.all(20),
+                      child:TextButton(
+  onPressed: () {},
+  style: TextButton.styleFrom(
+    padding: EdgeInsets.all(16.0),
+     backgroundColor:Colors.black , 
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.zero,
+    ),
+  ),
+  child: const Text('Register', style: TextStyle(color: Colors.white),),
+)
+                    ), 
+           
                   ],
                 ), 
               ), 
@@ -47,7 +92,72 @@ class _LandingpageState extends State<Landingpage> {
             ], 
           ),
         ), 
+
+        Divider(
+          color: const Color.fromARGB(255, 0, 0, 0),
+          thickness: 1,
+          indent: 10,
+          endIndent: 10,
+),
+   Container(
+    
+    child: Center(
+      child: Column(
+        children: [
+         SizedBox(height: 60),
+         Text('BRGHGMC\nExamination Portal', style: TextStyle(fontSize: 89, fontWeight: FontWeight.bold), textAlign: TextAlign.center,),
+         Text(' Professional Education, Training and Research Unit (PETRU)', style: TextStyle(fontSize: 20  , fontWeight: FontWeight.bold), textAlign: TextAlign.center,),
+        ],
+
+      ),
+
+    ), 
+   ),
+
+   Center(
+    child: Center(
+      child:Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+           Container(
+                      margin: EdgeInsets.all(20),
+                      child:TextButton(
+  onPressed: () {},
+  style: TextButton.styleFrom(
+    padding: EdgeInsets.all(16.0),
+     backgroundColor:Colors.black , 
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.zero,
+    ),
+  ),
+  child: const Text("I'm a Examinee", style: TextStyle(color: Colors.white),),
+)
+
+                    ), 
+
+                     Container(
+                      margin: EdgeInsets.all(20),
+                      child:TextButton(
+  onPressed: () {},
+  style: TextButton.styleFrom(
+    padding: EdgeInsets.all(16.0),
+     backgroundColor:Colors.black , 
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.zero,
+    ),
+  ),
+  child: const Text("I'm a Test Administrator", style: TextStyle(color: Colors.white),),
+)
+
+                    ), 
+        ],
+      ),
+    ),
+   ), 
       ],
+
+
+
      ), 
     );
   }
