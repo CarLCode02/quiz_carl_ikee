a9f271e986b30c7ed42f72c87c951d87d66a096f
Ike Renson Salcedo Landong <iklandong@my.cspc.edu.ph>
2026-04-14 09:25:16 +0800

    added a container

--- lib/homepage/landingpage.dart
+++ lib/homepage/landingpage.dart
@@ -10,6 +10,12 @@ class Landingpage extends StatefulWidget {
 class _LandingpageState extends State<Landingpage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+     body:Container(
+      child: Row(
+        
+      ), 
+     ), 
+    );
   }
 }
